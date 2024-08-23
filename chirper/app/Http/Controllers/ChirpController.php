237298a6a47b9557187a1cee5f4e26ea2ba68e93
mich785 <nderimichelle@gmail.com>
2024-08-23 10:55:40 +0300

    Added `use Illuminate\View\View;` statement to ChirpController.php.

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Gate;
 
